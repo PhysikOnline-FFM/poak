8eaee71001840e20dea86ae2b82518d57ef7b2b0
thomkeh <thomask175@gmail.com>
2014-07-19 01:11:41 +0200

    defined models

--- manage_worksheets/models.py
+++ manage_worksheets/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Tag(models.Model):
+    name = models.CharField(max_length=50)
+
+class Worksheet(models.Model):
+    ID = models.CharField(max_length=20)
+    title = models.CharField(max_length=200)
+    tags = models.ManyToManyField(Tag)
+    pub_date = models.DateTimeField(auto_now_add=True)
+    owner = models.CharField(max_length=20)
+    upvotes = models.IntegerField(default=0)
+    downvotes = models.IntegerField(default=0)
