41b1092e342026d7a3ab7d2fc6a91a71bf095133
Philip-A <junk@philip-arnold.de>
2014-10-30 18:34:56 +0100

    Abschliessende Aenderungen. Nun fehlt nur noch das passende Bootstrap-Theme

--- manage_worksheets/forms.py
+++ manage_worksheets/forms.py
@@ -3,8 +3,8 @@ from manage_worksheets.models import Tag
 
 class SubmissionForm(forms.Form):
     url = forms.URLField(widget=forms.URLInput(attrs={'class': 'form-control'}), max_length=200, label="Adresse (URL)")
-    tags = forms.ModelMultipleChoiceField(widget=forms.CheckboxSelectMultiple(attrs={'class': 'form-control'}), queryset=Tag.objects.all(), label="Schlagworte")
+    tags = forms.ModelMultipleChoiceField(widget=forms.CheckboxSelectMultiple(attrs={'class': ''}), queryset=Tag.objects.all(), label="Schlagworte")
     
 class ChooseTagsForm(forms.Form):
-    tags = forms.ModelMultipleChoiceField(widget=forms.CheckboxSelectMultiple(attrs={'class': 'form-control'}), queryset=Tag.objects.all(), label="Schlagworte")
+    tags = forms.ModelMultipleChoiceField(widget=forms.CheckboxSelectMultiple(attrs={'class': ''}), queryset=Tag.objects.all(), label="Schlagworte")
     worksheet_id = forms.CharField(widget=forms.HiddenInput)
--- manage_worksheets/urls.py
+++ manage_worksheets/urls.py
@@ -3,25 +3,19 @@ from django.conf.urls import patterns, url
 from manage_worksheets import views, ajax
 
 urlpatterns = patterns('',
-
     # html pages
     url(r'^$', views.main, name='main'),
     url(r'^submit$', views.submit, name='submit'),
     url(r'^choose_tags$', views.choose_tags, name='choose_tags'),
-    url(r'^w/(?P<worksheet_id>.*)/login$', views.loggedin_details,
-        name='loggedin_details'),
-    url(r'^w/(?P<worksheet_id>.*)/delete$', views.delete,
-        name='delete'),
+    url(r'^w/(?P<worksheet_id>.*)/login$', views.loggedin_details, name='loggedin_details'),
+    url(r'^w/(?P<worksheet_id>.*)/delete$', views.delete, name='delete'),
     url(r'^w/(?P<worksheet_id>.*)$', views.details, name='details'),
 
     # ajax functions
     url(r'^tags$', ajax.tags, name='tags'),
-    url(r'^worksheet_details/(?P<worksheet_id>.*)$',
-        ajax.worksheet_details, name='worksheet_details'),
-    url(r'^worksheet_list$',
-        ajax.worksheet_list, name='worksheet_list'),
-    url(r'^worksheets_for_tag/(?P<tag_id>\d+)', ajax.worksheets_for_tag,
-        name='worksheets_for_tag'),
+    url(r'^worksheet_details/(?P<worksheet_id>.*)$', ajax.worksheet_details, name='worksheet_details'),
+    url(r'^worksheet_list$', ajax.worksheet_list, name='worksheet_list'),
+    url(r'^worksheets_for_tag/(?P<tag_id>\d+)', ajax.worksheets_for_tag, name='worksheets_for_tag'),
     url(r'^minus_tag/(?P<tag_id>\d+)', ajax.minus_tag, name='minus_tag'),
     url(r'^move/(?P<from_id>\w+?)/(?P<to_id>\w+)', ajax.move, name='move'),
-    )
+)
